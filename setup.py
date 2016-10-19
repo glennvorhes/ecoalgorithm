3b2e772428e69a07179a842538b0504bd8f0e406
Glenn Vorhes <gavorhes@wisc.edu>
2016-10-18 19:25:47 -0500

    add install requires to setup

--- setup.py
+++ setup.py
@@ -5,9 +5,14 @@ setup(
     version='',
     packages=['ecoalgorithm'],
     package_data={'ecoalgorithm': ['static/*.js', 'static/*.map', 'static/*.css', 'templates/*.html']},
-    url='',
-    license='',
+    url='https://github.com/glennvorhes/ecoalgorithm',
+    license='MIT',
     author='Glenn Vorhes',
     author_email='glennvorhes@gmail.com',
-    description='Optimization algorithm to mimic competition between species'
+    description='Optimization algorithm to mimic competition between species',
+    install_requires=[
+        'sqlalchemy',
+        'flask',
+        'typing'
+    ]
 )
